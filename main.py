1eba9acf12ca34b1715e4e220f0f10cc17d6378d
filius-fall <sreeram596@gmail.com>
2026-01-26 13:02:52 +0530

    Working UDP and TCP connection logic

--- main.py
+++ main.py
@@ -1,19 +1,61 @@
-import socket
+import socket, threading
 import time
 
+
 def main():
-   
-    
+
     PORT = 37020
 
+    def get_ip():
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
+
+    def handle_tcp_connection(conn):
+        def recv_conn():
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(f"peer: {data.decode()}")
+
+        threading.Thread(target=recv_conn, daemon=True).start()
+        while True:
+            msg = input("> ")
+            conn.sendall(msg.encode())
+
+    def tcp_server():
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.bind(("0.0.0.0", 6001))
+        tcp.listen()
+        print("TCP listening on 6001")
+
+        while True:
+            conn, addr = tcp.accept()
+            print(f"TCP connected form {addr}")
+            handle_tcp_connection(conn)
+
+    threading.Thread(target=tcp_server, daemon=True).start()
+
+    my_ip = get_ip()
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", PORT))
-    
+
     print("Listening to the brodcast for new messages.....")
-    
+
     while True:
         data, addr = sock.recvfrom(4096)
-        print(f"Recieved data from {addr}: {data.decode()}") 
+        print(f"Recieved data from {addr}: {data.decode()}")
+        sender_ip = addr[0]
+        if sender_ip == my_ip:
+            print(f"Both have the same IP addr: {sender_ip}")
+            continue
+        tcp_port = int(data.decode().split("Port=")[1])
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.connect((sender_ip, tcp_port))
+        handle_tcp_connection(tcp)
 
 
 if __name__ == "__main__":
--- send_broadcast.py
+++ send_broadcast.py
@@ -1,19 +1,56 @@
-import socket
+import socket, threading
 import time
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+
+    return ip
+
 
 def main():
     BROADCAST_IP = "255.255.255.255"
     PORT = 37020
+    TCP_PORT = 5002
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    def handle_connection(conn, addr):
+        print(f"TCP connection accepted: {addr}")
+        while True:
+       	    data = conn.recv(1024)
+       	    if not data:
+       	        return
+       	    print("peer data is", data.decode()) 
+        msg = f"This is message from {my_ip} to {addr}"
+        print(msg)
+        conn.close()
 
-    while True:
-        message = b"Hello i am brodcasting this test data"
-        sock.sendto(message, (BROADCAST_IP,PORT))
-        print("Broadcast is sent")
-        time.sleep(1)
+    def tcp_server():
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.bind(("0.0.0.0", TCP_PORT))
+        tcp.listen()
+        print(f"Listening to port: {TCP_PORT}")
+
+        while True:
+            conn, addr = tcp.accept()
+
+            handle_connection(conn, addr)
+
+    def udp_server():
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+        while True:
+            message = f"Hello|Port=5002".encode()
+            sock.sendto(message, (BROADCAST_IP, PORT))
+            print("Broadcast is sent")
+            time.sleep(2)
+
+    threading.Thread(target=tcp_server, daemon=True).start()
+    udp_server()
+
 
 if __name__ == "__main__":
     main()
-
