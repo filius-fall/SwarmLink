afb053529cdb0f05f62050cfe6c24ea107118225
filius-fall <sreeram596@gmail.com>
2026-01-31 19:22:46 +0530

    feat: Implement UDP broadcasting for peer discovery and TCP connection setup.

--- udp_tcp_connection.py
+++ udp_tcp_connection.py
@@ -3,11 +3,20 @@ import threading
 
 def main():
 
-    PORT = 6002
+    UDP_PORT = 6002
+    BROADCAST_IP = "255.255.255.255"
+    TCP_PORT = 5002
+
+    def get_ip():
+        s = socket.socket(socket.socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
 
     def handle_tcp(addr,port, mode="Recv"):
 
-        tcp_sock = socket.socket(AF_INET, socket.SOCK_STREAM )
+        tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM )
         tcp.connect((addr,port))
 
         def recv_conn():
@@ -29,10 +38,10 @@ def main():
 
     
     def udp_listen_server():
-       udp_sock = socket.socket(AF_INET, socket.SOCK_DGRAM)
-       udp_sock.bind(("0.0.0.0",PORT))
+       udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+       udp_sock.bind(("0.0.0.0",UDP_PORT))
 
-       print("Listening on port:",PORT)
+       print("Listening on port:",UDP_PORT)
 
        while True:
             data,addr = udp_sock.recvfrom(4096)
@@ -43,5 +52,22 @@ def main():
 
             if ans == "y":
                 handle_tcp(addr,tcp_port,"Recv")
+                
+    threading.Thread(target=udp_listen_server, daemon=True).start()
+
+    def udp_send_server(message):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        ip = get_ip()
+
+        udp_message = f"Sending Message|Port={TCP_PORT}".encode()
+
+        while True:
+            sock.sendto(udp_message,(BROADCAST_IP,UDP_PORT))
+            print(f"Broadcast is being done on {UDP_PORT}")
+            time.sleep(10)
             
                 
+
+if __name__ == "__main__":
+    main()
